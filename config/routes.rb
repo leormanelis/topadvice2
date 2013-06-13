8f7b8abea5eefb5a28784ec8fe769df9ffc7c5d8
Leor Manelis <leormanelis@gmail.com>
2013-06-13 11:21:48 -0600

    added more resources

--- config/routes.rb
+++ config/routes.rb
@@ -55,7 +55,10 @@ TopAdvice::Application.routes.draw do
   # This is a legacy wild controller route that's not recommended for RESTful applications.
   # Note: This route will make all actions in every controller accessible via GET requests.
   # match ':controller(/:action(/:id))(.:format)'
+root :to => 'mains#index'
 resources :mains 
 resources :advices
-recources :contacts
+resource  :contact
+resource  :about
+resource  :mailing_list
 end
