6e753aa1d43f9d321200d3b69c0ed325d2f7fe5b
Leor Manelis <leormanelis@gmail.com>
2013-06-14 12:22:34 -0600

    updated psqlite to pg

--- app/models/advice.rb
+++ app/models/advice.rb
@@ -1,12 +1,5 @@
 class Advice < ActiveRecord::Base
     validates_presence_of :full_name
-#    full_name = full_name.new
-#      if full_name.save == true
-#      puts "Thanks for your subbmission"
-#      else
-#  for error in full_name.errors.full_messages
-#    puts error
-#  end
-#end
+    attr_accessible :full_name, :message
        
 end 
--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,9 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+Advice.create(full_name: "Bob", message: "Bob-Life is short so, appriciate.")
+Advice.create(full_name: "Josh", message: "Josh-Don't spill coffee on your computer.")
+Advice.create(full_name: "Daniel", message: "Daniel-Don't be a code cowboy and test your work!")
+Advice.create(full_name: "Luke", message: "Luke-Who needs testing? Be a code cowboy!")
+Advice.create(full_name: "Leor", message: "Leor-Never leave home without a snack.")
+Advice.create(full_name: "Seth", message: "Seth-It always looks better with flowers.")
