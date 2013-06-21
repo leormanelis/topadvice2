5e02cfdc5035a311c877c1c7c1ac7312bd86e94e
Leor Manelis <leormanelis@gmail.com>
2013-06-21 10:25:38 -0600

    got rid of rack flash

--- config/routes.rb
+++ config/routes.rb
@@ -1,60 +1,4 @@
-TopAdvice::Application.routes.draw do
-  # The priority is based upon order of creation:
-  # first created -> highest priority.
-
-  # Sample of regular route:
-  #   match 'products/:id' => 'catalog#view'
-  # Keep in mind you can assign values other than :controller and :action
-
-  # Sample of named route:
-  #   match 'products/:id/purchase' => 'catalog#purchase', :as => :purchase
-  # This route can be invoked with purchase_url(:id => product.id)
-
-  # Sample resource route (maps HTTP verbs to controller actions automatically):
-  #   resources :products
-
-  # Sample resource route with options:
-  #   resources :products do
-  #     member do
-  #       get 'short'
-  #       post 'toggle'
-  #     end
-  #
-  #     collection do
-  #       get 'sold'
-  #     end
-  #   end
-
-  # Sample resource route with sub-resources:
-  #   resources :products do
-  #     resources :comments, :sales
-  #     resource :seller
-  #   end
-
-  # Sample resource route with more complex sub-resources
-  #   resources :products do
-  #     resources :comments
-  #     resources :sales do
-  #       get 'recent', :on => :collection
-  #     end
-  #   end
-
-  # Sample resource route within a namespace:
-  #   namespace :admin do
-  #     # Directs /admin/products/* to Admin::ProductsController
-  #     # (app/controllers/admin/products_controller.rb)
-  #     resources :products
-  #   end
-
-  # You can have the root of your site routed with "root"
-  # just remember to delete public/index.html.
-  # root :to => 'welcome#index'
-
-  # See how all your routes lay out with "rake routes"
-
-  # This is a legacy wild controller route that's not recommended for RESTful applications.
-  # Note: This route will make all actions in every controller accessible via GET requests.
-  # match ':controller(/:action(/:id))(.:format)'
+TopAdvice::Application.routes.draw do  
 root      :to => 'mains#index'
 resources :mains 
 resources :advices
@@ -62,8 +6,8 @@ resource  :contact
 resource  :about
 resource  :mailing_list
 get  "/admin"       => "admin#login",      as: "login"
-post "/admin_login" => "admin#login_post", as: "login_post"
+post "/admin_login" => "admin#login_post", as: "admin_edit"
+get  "/admin_edit"  => "admin#admin_edit", as: "admin_edit"
 post "/logout"      => "admin#logout"
-get  "/admin_edit"  => "admin#edit",       as: "admin_edit"
 
 end
